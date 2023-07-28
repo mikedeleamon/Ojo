4668b804a93105d2956737a82bbbf696b5dad755
Dele Amon <deleamon@Deles-MBP.attlocal.net>
2023-07-27 23:51:32 -0500

    created settings, mock objects & functionality for outfit suggestions

--- src/components/WeatherDetails/WeatherDetails.js
+++ src/components/WeatherDetails/WeatherDetails.js
@@ -1,33 +1,37 @@
-import React, { useState, useEffect } from "react";
-import '../../App.css'
+import React, { useState, useEffect } from 'react';
+import '../../App.css';
+import OutfitSuggestion from '../OutfitSuggestion/OutfitSuggestion';
 
 const WeatherDetails = ({ weatherDetails }) => {
-const [temperature, setTemperature] = useState('');
-const [windSpeed, setWindSpeed] = useState('');
-const [humidity, setHumidity] = useState('');
-const [tempUnit, setTempUnit] = useState('')
-const [uvIndex, setUvIndex] = useState('');
-const [feelsLike, setFeelsLike] = useState('');
-useEffect(() => {
-if (Object.keys(weatherDetails).length > 0) {
-    setTemperature(weatherDetails[0].Temperature.Imperial.Value);
-    setWindSpeed(weatherDetails[0].Wind.Speed.Imperial.Value);
-    setHumidity(weatherDetails[0].RelativeHumidity);
-    setTempUnit(weatherDetails[0].Temperature.Imperial.Unit)
-    setUvIndex(weatherDetails[0].UVIndexText);
-    setFeelsLike(weatherDetails[0].RealFeelTemperature.Imperial.Value);
-}
-}, [weatherDetails]);
+    const [temperature, setTemperature] = useState('');
+    const [windSpeed, setWindSpeed] = useState('');
+    const [humidity, setHumidity] = useState('');
+    const [tempUnit, setTempUnit] = useState('');
+    const [uvIndex, setUvIndex] = useState('');
+    const [feelsLike, setFeelsLike] = useState('');
+    useEffect(() => {
+        if (Object.keys(weatherDetails).length > 0) {
+            setTemperature(weatherDetails[0].Temperature.Imperial.Value);
+            setWindSpeed(weatherDetails[0].Wind.Speed.Imperial.Value);
+            setHumidity(weatherDetails[0].RelativeHumidity);
+            setTempUnit(weatherDetails[0].Temperature.Imperial.Unit);
+            setUvIndex(weatherDetails[0].UVIndexText);
+            setFeelsLike(weatherDetails[0].RealFeelTemperature.Imperial.Value);
+        }
+    }, [weatherDetails]);
 
-return (
-<div>
-    <p>{`temperature: ${temperature}\u00B0 ${tempUnit}`} </p>
-    <p>wind speed: {windSpeed} mi/h</p>
-    <p>UV Index: {uvIndex}</p>
-    <p>humidity: {humidity}</p>
-    <p>feels like: {feelsLike} F</p>
-</div>
-);
+    return (
+        <>
+            <div>
+                <p>{`temperature: ${temperature}\u00B0 ${tempUnit}`} </p>
+                <p>wind speed: {windSpeed} mi/h</p>
+                <p>UV Index: {uvIndex}</p>
+                <p>humidity: {humidity}</p>
+                <p>feels like: {feelsLike} F</p>
+            </div>
+            <OutfitSuggestion />
+        </>
+    );
 };
 
 export default WeatherDetails;
--- src/views/Settings/Settings.js
+++ src/views/Settings/Settings.js
@@ -1,13 +1,74 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import '../../App.css';
-import styles from './Settings.module.css'
+import styles from './Settings.module.css';
 
+const Settings = ({ onSaveSettings }) => {
+    const [clothingStyle, setClothingStyle] = useState('Casual');
+    const [location, setLocation] = useState('New York');
+    // Add more state variables for other settings as needed
 
-const Settings = () => {
+    const handleSave = () => {
+        const settings = {
+            clothingStyle,
+            location,
+            // Add other settings to the object as needed
+        };
+
+        // Pass the settings object to the parent component (e.g., App.js)
+        onSaveSettings(settings);
+    };
+
     return (
-        <div className={`App`}>
-            <h1>Settings</h1>
+        <div className='App'>
+            <h2>Settings</h2>
+            <div>
+                <label>
+                    Preferred Clothing Style:
+                    <input
+                        type='text'
+                        value={clothingStyle}
+                        onChange={(e) => setClothingStyle(e.target.value)}
+                    />
+                </label>
+            </div>
+            <div>
+                <label>
+                    Location:
+                    <input
+                        type='text'
+                        value={location}
+                        onChange={(e) => setLocation(e.target.value)}
+                    />
+                </label>
+            </div>
+            <div>
+                <label>
+                    Preferred Clothing Style: {clothingStyle}
+                    <input
+                        type='range'
+                        min='0'
+                        max='100'
+                        value={clothingStyle}
+                        onChange={(e) => setClothingStyle(e.target.value)}
+                    />
+                </label>
+            </div>
+            <div>
+                <label>
+                    Location: {location}
+                    <input
+                        type='range'
+                        min='0'
+                        max='100'
+                        value={location}
+                        onChange={(e) => setLocation(e.target.value)}
+                    />
+                </label>
+            </div>
+            {/* Add more input fields for other settings as needed */}
+            <button onClick={handleSave}>Save</button>
         </div>
     );
 };
+
 export default Settings;
