871628652af4d57876f268d4b6647031ff1fdb53
Dele Amon <deleamon@Deles-MacBook-Pro.local>
2024-11-30 01:43:50 -0500

    coverted js to typescript

--- src/views/LoginPage/LoginPage.tsx
+++ src/views/LoginPage/LoginPage.tsx
@@ -3,20 +3,25 @@ import OjoLogoLight from '../../components/logos/OjoLogoLight/OjoLogoLight';
 import styles from './LoginPage.module.css';
 import { useNavigate } from 'react-router-dom';
 
-const LoginPage = ({ setLoggedIn }) => {
+interface LoginPageProps {
+    setLoggedIn: (value: boolean) => void;
+}
+
+const LoginPage: React.FC<LoginPageProps> = ({ setLoggedIn }) => {
     const navigate = useNavigate();
 
     const navigateToPage = () => {
         setLoggedIn(true);
         navigate('/');
     };
+
     const navigateToSignupPage = () => {
         navigate('/signup');
     };
 
     return (
         <div className={`${styles.center} mt-5`}>
-            <OjoLogoLight className='currentWeatherLogo ' />
+            <OjoLogoLight className='currentWeatherLogo' />
 
             <div className='col-8 col-sm-4 col-lg-2'>
                 <input
