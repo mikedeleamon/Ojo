a946b5303143b33ad510b5b69e70e57fd19cca18
Dele Amon <deleamon@Deles-MacBook-Pro.local>
2024-12-14 11:10:35 -0500

    pkg update

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-    render(<App />);
-    const linkElement = screen.getByText(/Ojo Weather App/i);
-    expect(linkElement).not.toBeInTheDocument();
+// Mock LoginPage to ensure it includes "Don't have an account"
+jest.mock('react-router-dom', () => ({
+    BrowserRouter: ({ children }: { children: React.ReactNode }) => (
+        <div>{children}</div>
+    ), // Mock BrowserRouter
+    Route: ({ children }: { children: React.ReactNode }) => (
+        <div>{children}</div>
+    ), // Mock Route
+    Switch: ({ children }: { children: React.ReactNode }) => (
+        <div>{children}</div>
+    ), // Mock Switch
+    useHistory: jest.fn(), // Mock any necessary hooks (e.g., useHistory)
+}));
+
+test('renders login page', () => {
+    render(
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    );
+    const linkElement = screen.getByText(/Don't have an account/i);
+    expect(linkElement).toBeInTheDocument();
 });
